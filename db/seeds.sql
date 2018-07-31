a10f84a3309a40a492015cda1b42ce470d1f5c01
Kelby Behounek <knbehounek@hotmail.com>
2018-07-31 12:33:42 -0700

    inital setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Classic Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Classic Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
