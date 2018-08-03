d0b1f0e61a17204d93016c96c3db01bd3be1e5ff
Kelby Behounek <knbehounek@hotmail.com>
2018-08-03 12:03:46 -0700

    schema final update

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
+CREATE TABLE burgers(
+id int AUTO_INCREMENT,
+PRIMARY KEY (id),
+burger_name varchar(50) NOT NULL,
+devoured BOOLEAN default false,
+date TIMESTAMP);
