4133c2f9383889313c6f1a9ea139935a85ea0b02
Kelby Behounek <knbehounek@hotmail.com>
2018-08-03 10:10:40 -0700

    schema fix

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,3 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
+CREATE DATABASE todolist;
+USE todolist;
 
-CREATE TABLE burgers(
-id int AUTO_INCREMENT,
-PRIMARY KEY (id),
-burger_name varchar(50) NOT NULL,
-devoured BOOLEAN default false,
-date TIMESTAMP);
